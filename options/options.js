79d890f9bf96c1e3621ba39e6d49291601879908
Will Starling <willstar2003@gmail.com>
2023-05-12 02:50:07 -0500

    Commit 8 - 5/12/23
    The GitHub website was updated, so the commit message and description no longer work when set through the URL.
    I am now switching to using the OctoKit library. I would've used this from the start, but I thought it would've taken a lot longer.
    - Updated options page to include input for auth token
    - Cleaned up some code in the options page; made one collective save button for all options

--- options/options.js
+++ options/options.js
@@ -1,59 +1,112 @@
-// Restores the options to what was last selected
+////////// Restores all options to last save (on refresh) //////////
 const restoreOptions = () => {
   chrome.storage.sync.get(
-    {"github-repo-name" : "github-repo-name"}, //, likesColor: true },
+    {"github-repo-name" : "github-repo-name", "github-token" : "github-token"}, //, github-commit-preview-checkbox: true },
     (items) => {
       console.log(items)
+
       document.getElementById('github-repo-name').placeholder = items["github-repo-name"];
       document.getElementById('github-repo-name').value = items["github-repo-name"];
-      //document.getElementById('like').checked = items.likesColor;
+
+      document.getElementById('github-token').placeholder = "•".repeat(items["github-token"].length);
+      document.getElementById('github-token').value = items["github-token"];
+
+      //document.getElementById('github-commit-preview-checkbox').checked = items.github-commit-preview-checkbox;
     }
   );
 };
-//// Saves the options
-//const saveOptions = () => {
-//  chrome.storage.sync.set(
-//    { github_repo_name : document.getElementById('github-repo-name').value},
-//    () => {
-//      // Update status to let user know options were saved.
-//      document.getElementById('github-repo-name').placeholder = document.getElementById('github-repo-name').value;
-//      const status = document.getElementById('status2');
-//      status.textContent = 'Options saved.';
-//      setTimeout(() => {
-//        status.textContent = '';
-//      }, 2000);
-//    }
-//  );
-//};
+
+////////// Saves options (via save button) //////////
+const saveOptions = () => {
+  saveGitHubRepoName();
+  saveGitHubAuthToken();
+  
+  
+  
+  //chrome.storage.sync.set(
+  //  { github_repo_name : document.getElementById('github-repo-name').value},
+  //  () => {
+  //    // Update status to let user know options were saved.
+  //    document.getElementById('github-repo-name').placeholder = document.getElementById('github-repo-name').value;
+  //    const status = document.getElementById('status2');
+  //    status.textContent = 'Options saved.';
+  //    setTimeout(() => {
+  //      status.textContent = '';
+  //    }, 2000);
+  //  }
+  //);
+};
 
 
 document.addEventListener('DOMContentLoaded', restoreOptions); // On refresh, restore options' values
-//document.getElementById('save-repo-name').addEventListener('click', saveOptions); // Save options on "save" button click
+document.getElementById('save-options-button').addEventListener('click', saveOptions); // Save options on "save" button click
 
 
-// Save typed GitHub repository name
-document.querySelector("#save-repo-name").addEventListener("click", function() {
+///// Save GitHub repository name /////
+function saveGitHubRepoName(){
+  // Store typed repo name and get status text (to notify of save status)
   const github_repo = document.querySelector("#github-repo-name").value;
-  let status = document.getElementById("status");
-  console.log("Repo name: " + document.querySelector("#github-repo-name").value);
+  const status = document.getElementById("save-repo-status");
+  console.log("Typed repo name: " + github_repo);
 
-  // Update status text
-  if (github_repo === "" || !(/^[a-zA-Z_.-]+$/.test(github_repo))){ // repo name cannot be blank and must only contain valid repo name characters
-    status.style.color = "#f22c21";
-    status.textContent = "Repository names cannot be blank and must be characters \"a-z\", \"A-Z\", \"_\", \".\", and \"-\". Please Try again.";
-    
+  // Check if typed text is new
+  if (github_repo !== document.querySelector("#github-repo-name").placeholder){
+    // Check if repo name is valid; update status text
+    if (github_repo !== "" && /^[a-zA-Z_.-]+$/.test(github_repo)){ // Repo name cannot be blank and must only contain valid repo name characters
+      // Save repo name (to storage and placeholder) and print success
+      chrome.storage.sync.set( {"github-repo-name": github_repo} );
+      document.getElementById('github-repo-name').placeholder = github_repo;
+      status.style.color = "#00be00";
+      status.textContent = "Repo name saved!";
+    } else {
+      // Print repo name constraints
+      status.style.color = "#f22c21";
+      status.textContent = "Repo names cannot be blank and can only contain characters \"a-z\", \"A-Z\", \"_\", \".\", and \"-\". Please Try again.";
+    }
+  
+    // Clear the status text
     setTimeout(() => {
       status.textContent = "";
-    }, 2000);
-  } else{
-    // Repo name was saved successfully
-    status.style.color = "#00be00";
-    status.textContent = "Repository name for LeetCode solutions successfully saved!";
-
-    // Save name to chrome storage
-    chrome.storage.sync.set( {"github-repo-name": document.querySelector("#github-repo-name").value} );
-    setTimeout(() => {
-      status.textContent = "";
-    }, 2000);
+    }, 3000);
   }
-});
+};
+
+///// Save GitHub authentication token /////
+function saveGitHubAuthToken(){
+  // Store typed token and get status text (to notify of save status)
+  const github_token = document.querySelector("#github-token").value;
+  const status = document.getElementById("save-github-token-status");
+  console.log("Typed auth token: " + github_token);
+
+    // Make sure it doesn't equal the last token
+    chrome.storage.sync.get("github-token", function (token){
+      if (("github-token" in token && github_token === "" && token["github-token"] !== github_token)){ // Token is empty
+        // Delete the token
+        chrome.storage.sync.set( {"github-token": ""} );
+        document.getElementById('github-token').placeholder = "";
+        status.style.color = "#ebae34";
+        status.textContent = "Token deleted!";
+
+        // Clear the status text
+        setTimeout(() => {
+          status.textContent = "";
+        }, 3000);
+      }
+      else if (token["github-token"] !== github_token){ // Token doesn't equal last submission
+        // Save token (to storage and placeholder)
+        chrome.storage.sync.set( {"github-token": github_token} );
+        document.getElementById('github-token').placeholder = "•".repeat(github_token.length);
+        status.style.color = "#00be00";
+        status.textContent = "Token saved!";
+        
+        // Clear the status text
+        setTimeout(() => {
+          status.textContent = "";
+        }, 3000);
+      }
+      // do nothing otherwise (if it is equal to the last submission)
+    });
+
+    
+  
+};
