c9e9efcbd8a4162c472e370e12094ec3d2eeadf8
Will Starling (Macbook Air) <willstar2003@gmail.com>
2025-08-22 01:08:15 -0400

    Renamed extension and cleaned up popup html/css

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,3 @@
-// Options button
 document.querySelector("#options").addEventListener("click", function() {
   chrome.runtime.openOptionsPage();
-});
-
-// Help button (not yet implemented)
-//document.querySelector("#help").addEventListener("click", function() {
-//  window.open(chrome.runtime.getURL("../help/help.html"));
-//});
-
-// Social links
-document.querySelector("#github-links").addEventListener("click", function() {
-  window.open("https://github.com/Willisaur");
-});
-document.querySelector("#linkedin-links").addEventListener("click", function() {
-  window.open("https://www.linkedin.com/in/willstarling/");
-});document.querySelector("#paypal-links").addEventListener("click", function() {
-  window.open("https://paypal.me/willisaur");
 });
